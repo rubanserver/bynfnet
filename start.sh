b341c0a14537d18e3cd6740a8ce57e4c02b612ff
tyrionfiles <133563974+tyrionfiles@users.noreply.github.com>
2023-06-18 22:10:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/JayRaj8833/JS-link-bot.git
+git clone https://github.com/tyrionfiles/link.git
 cd /JS-link-bot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
